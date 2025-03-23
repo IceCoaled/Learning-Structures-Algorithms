1a59fdc21a72a2cab14d7ead61f6da3f03b1b4cb
Blake <146580877+IceCoaled@users.noreply.github.com>
2025-03-23 07:58:36 -0400

    for new stuff

--- Main.cpp
+++ Main.cpp
@@ -1,14 +1,27 @@
 #include "Sorting.hpp"
-//#include "Searching.hxx"
+//#include "Searching.hpp"
+//#include "SortingTests.h"
+
+
 
 
 int main()
 {	
-	auto* sAlgoS = new SortingAlgorithms<int>();
+	/// Our sorting algorithms class
+	
+	// auto* sAlgoS = new SortingAlgorithms<int>();
+	// sAlgoS->TestAllAlgorithms();
+	// delete sAlgoS;
+	
+	
+	///-------------------------------------------------------///
+	/// The tests for each sorting algorithm in sorting class
 
-	sAlgoS->TestAllAlgorithms();
-
-	delete sAlgoS;
+        // TestBubbleSort();
+	// TestSelectionSort();
+	// TestInsertionSort();
+	// TestQuickSort();
+	// TestMergeSort();
 
 	system( "pause" );
     return 0;
