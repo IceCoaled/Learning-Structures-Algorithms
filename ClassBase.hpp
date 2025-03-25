8ebb34701068d7d685cbbf90ae2f43edcb26c6de
Blake <146580877+IceCoaled@users.noreply.github.com>
2025-03-25 06:29:24 -0400

    Added linked list base class
    This uses string and entry number for sorting/ searching possibilities.

--- ClassBase.hpp
+++ ClassBase.hpp
@@ -10,7 +10,9 @@
 #include <limits>
 #include <numeric>
 #include <type_traits>
-
+#include <memory>
+#include <cassert>
+#include <__msvc_string_view.hpp>
 
 template < typename T>
 concept NumericConstraint = 
@@ -199,5 +201,246 @@ public:
 		std::cout << sortName << std::endl;
 	}
 };
+
+
+
+template< typename T >
+concept StringType = std::is_same_v< T, std::string > || 
+std::is_same_v< T, std::wstring >;
+
+
+template< typename T >
+	requires StringType< T >
+struct StringNode
+{
+	T name;
+	int entryNum;
+	StringNode* flink;
+	StringNode* blink;
+
+	/// <summary>
+	/// Default constructor
+	/// </summary>
+	StringNode(): entryNum( 0 ), flink( nullptr ), blink( nullptr ) {}
+
+	/// <summary>
+	/// New entry constructor
+	/// </summary>
+	/// <param name="entryNumber"></param>
+	/// <param name="newFlink"></param>
+	/// <param name="newBlink"></param>
+	StringNode( int entryNumber,T newName ,StringNode* newBlink ): 
+		entryNum( entryNumber ), name( newName ), flink( nullptr ), blink( newBlink ) {}
+};
+
+
+
+
+template< typename T >
+	requires StringType< T >
+class LinkListBase
+{
+protected:
+	/// <summary>
+	/// Head of our linked list
+	/// </summary>
+	StringNode<T>* head;
+
+	/// <summary>
+	/// Current entry of our list
+	/// This helps use with adding new entries
+	/// </summary>
+	StringNode<T>* current;
+
+	/// <summary>
+	/// Number of entries in our list
+	/// </summary>
+	int numOfEntries;
+
+	/// <summary>
+	/// Flag to tell if list is wrapped
+	/// Back to head
+	/// </summary>
+	bool isListWrapped;
+
+public:
+
+	LinkListBase(): head( nullptr ), current( nullptr ), numOfEntries( 0 ), isListWrapped( false )
+	{
+		InitHead();
+	}
+	~LinkListBase()
+	{
+		RemoveAllEntries();
+	}
+
+
+	/// <summary>
+	/// Initializes the head of the linked list
+	/// Sets current to head
+	/// </summary>
+	/// <returns></returns>
+	bool InitHead()
+	{
+		// Allocate the head
+		head = new StringNode<T>();
+		if ( head == nullptr )
+		{
+			// Return false if allocation failed
+			return false;
+		}
+		// Set current to the head
+		current = head
+		return true;
+	}
+
+
+	/// <summary>
+	/// Adds new entry to linked list
+	/// </summary>
+	/// <param name="name"></param>
+	/// <returns></returns>
+	bool AddEntry( T name )
+	{
+		// Allocate new entry
+		current->flink = new StringNode<T>( ++numOfEntries, name, current );
+		if ( current->flink == nullptr )
+		{
+			// Return false if allocation failed
+			return false;
+		}
+		// Push current forward to new entry
+		current = current->flink;
+		// If the list is to be wrapped we reset the head
+		if ( isListWrapped )
+		{
+			current->flink = head;
+		}		
+		return true;
+	}
+
+	/// <summary>
+	/// Print the entry number and name 
+	/// Of each entry in the linked list
+	/// </summary>
+	void PrintAllEntries()
+	{
+		// Set our loop entry to the first
+		// Entry in the list
+		StringNode<T>* entry = head->flink;
+
+		// If the list is wrapped we can use the
+		// Head as our stop signal
+		// else we just check for nullptr
+		if ( isListWrapped )
+		{
+			while ( entry != head )
+			{
+				// Print entry details
+				std::cout << "==============\n";
+				std::cout << "Entry number : " << entry->entryNum << "\n";
+				std::cout << "Entry name : " << entry->name << "\n";
+				std::cout << "==============\n";
+				// Push our entry to the next one
+				entry = entry->flink;
+			}
+		} else
+		{
+			while ( entry != nullptr )
+			{
+				// Print entry details
+				std::cout << "==============\n";
+				std::cout << "Entry number : " << entry->entryNum << "\n";
+				std::cout << "Entry name : " << entry->name << "\n";
+				std::cout << "==============\n";
+				// Push our entry to the next one
+				entry = entry->flink;
+			}
+		}
+	}
+
+
+	/// <summary>
+	/// This deletes all entries in the linked list
+	/// Including the head and resets count to zero
+	/// </summary>
+	void RemoveAllEntries()
+	{
+		// Set our loop entry to the second
+		// Entry in the list
+		StringNode<T>* entry = head->flink;
+
+		// If the list is wrapped we can use the
+		// Head as our stop signal
+		// else we just check for nullptr
+		if ( isListWrapped )
+		{
+			while ( entry != head )
+			{
+				// We delete the entries behind
+				// Us as we move forward through list
+				delete entry->blink;
+				// Push our entry to the next one
+				entry = entry->flink;
+			}
+		} else
+		{
+			while ( entry != nullptr )
+			{
+				// We delete the entries behind
+				// Us as we move forward through list
+				delete entry->blink;
+				// Push our entry to the next one
+				entry = entry->flink;
+			}
+		}		
+		// Delete head to finish
+		delete head;
+		head = nullptr;
+		current = nullptr;
+		numOfEntries = 0;
+	}
+
+	/*
+	* Abstract functions for derived classes
+	* To implement as we can add sorting and
+	* Searching algorithms to help these functions
+	*/
+	virtual void SortEntriesByName() = 0;
+	virtual T RemoveEntry( T name ) = 0;
+	virtual T RemoveEntry( int entryNum ) = 0;
+	virtual void RemoveEntry( T name ) = 0;
+	virtual void RemoveEntry( int entryNum ) = 0;
+	virtual void FlipList() = 0;
+
+protected:
+
+	/// <summary>
+	/// Print the entries details
+	/// We do a null check as this pointer will be
+	/// Passed in from outside this class
+	/// </summary>
+	/// <param name="entry"></param>
+	/// <returns></returns>
+	bool PrintEntryDetails( StringNode<T>* entry ) const
+	{
+		// Null check
+		if ( entry == nullptr )
+		{
+			return false;
+		}
+
+		// Print entry details
+		std::cout << "==============\n";
+		std::cout << "Entry number : " << entry->entryNum << "\n";
+		std::cout << "Entry name : " << entry->name << "\n";
+		std::cout << "==============\n";
+
+		return true;
+	}
+};
+
+
+
 #endif // !CLASSBASE_HXX
 
