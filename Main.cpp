fac789d392f9f14efedfa83430d356618fc05fe5
Blake <146580877+IceCoaled@users.noreply.github.com>
2025-03-30 08:32:17 -0400

    Update Main.cpp
    code from new additions

--- Main.cpp
+++ Main.cpp
@@ -1,13 +1,18 @@
 
 #include "Sorting.hpp"
 #include "Searching.hpp"
-
-
+#include "LinkedList.hpp"
+#include "IntrinsicCheck.hpp"
 
 
 
 int main()
 {	
+	if ( !CheckRequiredFeatures() )
+	{
+		throw std::runtime_error( "Failed AVX2 or BMI1 support check" );
+	}
+	
 	/// Our sorting algorithms class	
 	//auto sortAlgoS = std::make_unique< SortingAlgorithms< int > >();
 	//sortAlgoS->TestAllAlgorithms();
@@ -17,6 +22,22 @@ int main()
 	//auto searchAlgoS = std::make_unique< SearchAlogrithms< int > >();
 	//searchAlgoS->TestAllSearchAlgorithms();
 
+	// Our linked list algorithmns class
+	auto test = std::make_unique<LinkedListAlgorithms<std::string>>( true );
+	auto llS = std::make_unique< LinkedListAlgorithms< std::string > >( true );
+	llS->PrintAllEntries();
+	auto szList = std::optional<std::size_t>( 0 );
+	auto middleNode = std::expected<StringNode<std::string>*, std::string>( llS->GetListMiddle( szList ) );
+	if ( middleNode.has_value() )
+	{
+		std::println( "Middle List Node Name: {}, Entry Number: {}, List Size: {}", middleNode.value()->name, middleNode.value()->entryNum, szList.value());
+	} else
+	{
+		std::println( "ERROR: {}", middleNode.error() );
+	}
+
+	//llS->MergeSortEntries();
+	
 	system( "pause" );
     return 0;
 }
