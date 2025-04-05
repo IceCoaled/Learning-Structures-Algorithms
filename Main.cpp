d0c1b6acf8476143bd9461b2b66a4915b56a8064
Blake <146580877+IceCoaled@users.noreply.github.com>
2025-04-05 09:03:46 -0400

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -7,6 +7,7 @@
 
 int main()
 {	
+	
 	/// Our sorting algorithms class	
 	//auto sortAlgoS = std::make_unique< SortingAlgorithms< int > >();
 	//sortAlgoS->TestAllAlgorithms();
@@ -16,22 +17,10 @@ int main()
 	//auto searchAlgoS = std::make_unique< SearchAlogrithms< int > >();
 	//searchAlgoS->TestAllSearchAlgorithms();
 
-	// Our linked list algorithmns class
-	auto test = std::make_unique<LinkedListAlgorithms<std::string>>( true );
-	auto llS = std::make_unique< LinkedListAlgorithms< std::string > >( true );
-	llS->PrintAllEntries();
-	auto szList = std::optional<std::size_t>( 0 );
-	auto middleNode = std::expected<StringNode<std::string>*, std::string>( llS->GetListMiddle( szList ) );
-	if ( middleNode.has_value() )
-	{
-		std::println( "Middle List Node Name: {}, Entry Number: {}, List Size: {}", middleNode.value()->name, middleNode.value()->entryNum, szList.value());
-	} else
-	{
-		std::println( "ERROR: {}", middleNode.error() );
-	}
+	/// Our linked list algorithmns class	
+	//auto linkedListAlgos = std::make_unique< LinkedListAlgorithms< std::string > >( true );
+	//linkedListAlgos->RunClassFunctions();
 
-	//llS->MergeSortEntries();
-	
 	system( "pause" );
     return 0;
-}
+}							
